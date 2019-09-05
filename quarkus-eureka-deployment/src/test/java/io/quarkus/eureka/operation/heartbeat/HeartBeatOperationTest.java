5d0e3f418a70f81b5acc38ed3fcae42b74bdce74
francisco <fmcejudo@gmail.com>
2019-09-05 23:02:23 +0200

    Supporting heartBeat and delete operation with integration tests

--- quarkus-eureka-deployment/src/test/java/io/quarkus/eureka/operation/heartbeat/HeartBeatOperationTest.java
+++ quarkus-eureka-deployment/src/test/java/io/quarkus/eureka/operation/heartbeat/HeartBeatOperationTest.java
@@ -1,11 +1,98 @@
 package io.quarkus.eureka.operation.heartbeat;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import io.quarkus.eureka.client.InstanceInfo;
+import io.quarkus.eureka.config.InstanceInfoContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configure;
+import static com.github.tomakehurst.wiremock.client.WireMock.put;
+import static com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static io.quarkus.eureka.util.HostNameDiscovery.getHostname;
 
 public class HeartBeatOperationTest {
 
+    private WireMockServer wireMockServer;
+
+    private String serverUrl;
+
+    private HeartBeatOperation heartBeatOperation;
+
+    @BeforeEach
+    public void setUp() {
+        heartBeatOperation = new HeartBeatOperation();
+        this.wireMockServer = new WireMockServer(0);
+        wireMockServer.start();
+
+        this.serverUrl = String.format("http://localhost:%d", wireMockServer.port());
+    }
+
     @Test
     public void shouldUpdateInstanceWithPut() {
+        //Given
+        final String updatePath = "/eureka/apps/SAMPLE/" + getHostname();
+        wireMockServer.stubFor(put(urlEqualTo(updatePath))
+                .willReturn(aResponse().withStatus(200)));
+
+        InstanceInfo instanceInfo = InstanceInfo.of(TestInstanceInfoContext.of("SAMPLE", wireMockServer.port()));
+
+        //When
+        heartBeatOperation.heartbeat(serverUrl.concat("/eureka"), instanceInfo);
+
+        //Then
+        wireMockServer.verify(1, putRequestedFor(urlEqualTo(updatePath)));
 
     }
+
+    static class TestInstanceInfoContext implements InstanceInfoContext {
+
+        private final String name;
+        private final int port;
+
+        private TestInstanceInfoContext(final String name,
+                                        final int port) {
+            this.name = name;
+            this.port = port;
+        }
+
+        public static InstanceInfoContext of(final String name,
+                                             final int port) {
+            return new TestInstanceInfoContext(name, port);
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public int getPort() {
+            return port;
+        }
+
+        @Override
+        public String getVipAddress() {
+            return name;
+        }
+
+        @Override
+        public String getHealthCheckUrl() {
+            return "/info/health";
+        }
+
+        @Override
+        public String getHomePageUrl() {
+            return "/";
+        }
+
+        @Override
+        public String getStatusPageUrl() {
+            return "/info/status";
+        }
+    }
+
 }
---
+++ quarkus-eureka-deployment/src/test/java/io/quarkus/eureka/operation/remove/RemoveInstanceOperationTest.java
@@ -0,0 +1,48 @@
+package io.quarkus.eureka.operation.remove;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.delete;
+import static com.github.tomakehurst.wiremock.client.WireMock.deleteRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static io.quarkus.eureka.util.HostNameDiscovery.getHostname;
+
+class RemoveInstanceOperationTest {
+
+    private RemoveInstanceOperation removeInstanceOperation;
+
+    private WireMockServer wireMockServer;
+
+    private String serverUrl;
+
+
+    @BeforeEach
+    public void setUp() {
+
+        this.removeInstanceOperation = new RemoveInstanceOperation();
+        this.wireMockServer = new WireMockServer(0);
+        wireMockServer.start();
+
+        this.serverUrl = String.format("http://localhost:%d", wireMockServer.port());
+
+    }
+
+    @Test
+    public void shouldCallDeleteToRemoveInstance() {
+        //Given
+        final String deletePath = "/eureka/apps/SAMPLE/" + getHostname();
+        wireMockServer.stubFor(delete(urlEqualTo(deletePath))
+                .willReturn(aResponse().withStatus(200)));
+
+
+        //When
+        removeInstanceOperation.remove(serverUrl.concat("/eureka"), "SAMPLE");
+
+        //Then
+        wireMockServer.verify(1, deleteRequestedFor(urlEqualTo(deletePath)));
+    }
+
+}
